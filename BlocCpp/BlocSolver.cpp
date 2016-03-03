181537603658f78f23b2e11c982b0048b1c79e2b
Christoph Hausner <christoph_hausner@hotmail.de>
2016-03-03 01:59:43 +0100

    Switches to unordered_set

--- BlocCpp/BlocSolver.cpp
+++ BlocCpp/BlocSolver.cpp
@@ -7,19 +7,13 @@ void BlocSolver::Solve(BlockGrid& blockGrid, unsigned int smallestGroupSize, uns
 
 	unsigned int numberOfBlocks = blockGrid.GetNumberOfBlocks();
 
-	unsigned int hash = blockGrid.GetHash();
-
 	worstNumberOfBlocks = numberOfBlocks;
-	worstHash = hash;
 	
 	blockGrids.clear();
 	blockGrids.resize(numberOfBlocks + 1);
 
-	blockGrids[numberOfBlocks] = new std::vector<std::vector<BlockGrid>*>();	
-	blockGrids[numberOfBlocks]->resize(hash + 1);
-
-	(*blockGrids[numberOfBlocks])[hash] = new std::vector<BlockGrid>();
-	(*blockGrids[numberOfBlocks])[hash]->push_back(blockGrid);
+	blockGrids[numberOfBlocks] = new std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>();	
+	blockGrids[numberOfBlocks]->insert(blockGrid);
 
 	bestGrid = nullptr;
 	bestScore = UINT_MAX;
@@ -54,36 +48,27 @@ void BlocSolver::Solve(BlockGrid& blockGrid, unsigned int smallestGroupSize, uns
 
 void BlocSolver::SolveDepth(unsigned int maxDBSize, bool& stop, bool dontAddToDB)
 {
-	std::vector<std::vector<std::vector<BlockGrid>*>*> newBlockGrids;
+	std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*> newBlockGrids;
 
 	newBlockGrids.resize(worstNumberOfBlocks + 1);
 
 	unsigned int blockGridsSolved = 0;
 	unsigned int newDBSize = 0;
 	unsigned int newWorstNumberOfBlocks = 0;
-	unsigned int newWorstHash = 0;
 
 	for (int i = 0; i < blockGrids.size(); i++)
 		if (blockGrids[i] != nullptr)
 		{
-			for (int j = 0; j < blockGrids[i]->size(); j++)
-			{
-				if ((*blockGrids[i])[j] != nullptr)
+		    if (!stop && newDBSize < maxDBSize)
+				for (const BlockGrid& blockGrid : *(blockGrids[i]))
 				{
-					if (!stop && newDBSize < maxDBSize)
-						for (int k = 0; k < (*blockGrids[i])[j]->size(); k++)
-						{
-							newDBSize += SolveBlockGrid((*(*blockGrids[i])[j])[k], i, j, newBlockGrids, newWorstNumberOfBlocks, newWorstHash, stop, dontAddToDB);
+					newDBSize += SolveBlockGrid(blockGrid, i, newBlockGrids, newWorstNumberOfBlocks, stop, dontAddToDB);
 
-							blockGridsSolved++;
+					blockGridsSolved++;
 
-							if (stop || newDBSize >= maxDBSize)
-								break;
-						}
-
-					delete (*blockGrids[i])[j];
+					if (stop || newDBSize >= maxDBSize)
+						break;
 				}
-			}
 
 			delete blockGrids[i];
 		}
@@ -95,12 +80,11 @@ stop:
 	blockGrids = newBlockGrids;
 	dbSize = newDBSize;
 	worstNumberOfBlocks = newWorstNumberOfBlocks;
-	worstHash = newWorstHash;
 }
 
-unsigned int BlocSolver::SolveBlockGrid(const BlockGrid& blockGrid, unsigned int numberOfBlocks, unsigned int hash, 
-	std::vector<std::vector<std::vector<BlockGrid>*>*>& newBlockGrids, 
-	unsigned int& newWorstNumberOfBlocks, unsigned int& newWorstHash, bool& stop, bool dontAddToDB)
+unsigned int BlocSolver::SolveBlockGrid(const BlockGrid& blockGrid, unsigned int numberOfBlocks, 
+	std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*>& newBlockGrids, 
+	unsigned int& newWorstNumberOfBlocks, bool& stop, bool dontAddToDB)
 {
 	std::vector<std::vector<Position>> groups = blockGrid.GetGroups(smallestGroupSize);
 
@@ -130,13 +114,6 @@ unsigned int BlocSolver::SolveBlockGrid(const BlockGrid& blockGrid, unsigned int
 		if (newNumberOfBlocks > newWorstNumberOfBlocks)
 			newWorstNumberOfBlocks = newNumberOfBlocks;
 
-		BlockColor groupColor = bg.Blocks[groups[i][0].Y * bg.Width + groups[i][0].X];
-
-		int newHash = hash - (HASH_FACTORS[(int)groupColor] * groups[i].size());
-
-		if (newHash > newWorstHash)
-			newWorstHash = newHash;
-
 		if (newNumberOfBlocks == 0)
 		{
 			bg.Solution.push_back(i);
@@ -154,10 +131,10 @@ unsigned int BlocSolver::SolveBlockGrid(const BlockGrid& blockGrid, unsigned int
 		bg.RemoveGroup(groups[i]);
 
 		if (!dontAddToDB)
-			if (!IsInDatabase(bg, newNumberOfBlocks, newHash, newBlockGrids))
+			if (!IsInDatabase(bg, newNumberOfBlocks, newBlockGrids))
 			{
 				bg.Solution.push_back(i);
-				(*newBlockGrids[newNumberOfBlocks])[newHash]->push_back(bg);
+				newBlockGrids[newNumberOfBlocks]->insert(bg);
 
 				c++;
 			}
@@ -166,25 +143,14 @@ unsigned int BlocSolver::SolveBlockGrid(const BlockGrid& blockGrid, unsigned int
 	return c;
 }
 
-bool BlocSolver::IsInDatabase(const BlockGrid& blockGrid, unsigned int numberOfBlocks, unsigned int hash, 
-	std::vector<std::vector<std::vector<BlockGrid>*>*>& newBlockGrids) const
+bool BlocSolver::IsInDatabase(const BlockGrid& blockGrid, unsigned int numberOfBlocks,
+	std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*>& newBlockGrids) const
 {
 	if (newBlockGrids[numberOfBlocks] == nullptr)
 	{
-		newBlockGrids[numberOfBlocks] = new std::vector<std::vector<BlockGrid>*>();
-		newBlockGrids[numberOfBlocks]->resize(worstHash + 1);
+		newBlockGrids[numberOfBlocks] = new std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>();
+	    return false;
 	}
 
-	if ((*newBlockGrids[numberOfBlocks])[hash] == nullptr)
-		(*newBlockGrids[numberOfBlocks])[hash] = new std::vector<BlockGrid>();
-	else
-	{
-		std::vector<BlockGrid>& bgs = *(*newBlockGrids[numberOfBlocks])[hash];
-
-		for (int i = 0; i < bgs.size(); i++)
-			if (bgs[i].Equals(blockGrid))
-				return true;
-	}
-
-	return false;
+	return newBlockGrids[numberOfBlocks]->count(blockGrid) != 0;
 }
--- BlocCpp/BlocSolver.h
+++ BlocCpp/BlocSolver.h
@@ -2,20 +2,50 @@
 
 #include <memory>
 #include <vector>
+#include <unordered_set>
 #include "BlockGrid.h"
+
+struct BlockGridHash
+{
+	size_t operator()(const BlockGrid& blockGrid) const
+	{
+#if SIZE_MAX == UINT32_MAX
+		size_t hash = 2166136261;
+
+		for (int i = 0; i < blockGrid.Width * blockGrid.Height; i++)
+			hash = (hash ^ (int)blockGrid.Blocks[i]) * 16777619;
+
+		return hash;
+#elif SIZE_MAX == UINT64_MAX
+		size_t hash = 14695981039346656037;
+
+		for (int i = 0; i < blockGrid.Width * blockGrid.Height; i++)
+		    hash = (hash ^ (int)blockGrid.Blocks[i]) * 1099511628211;
+
+		return hash;
+#endif
+	}
+};
+
+struct BlockGridEqualTo
+{
+	bool operator()(const BlockGrid& blockGrid1, const BlockGrid& blockGrid2) const
+	{
+		return blockGrid1.Equals(blockGrid2);
+	}
+};
 
 class BlocSolver
 {
 	unsigned int smallestGroupSize;
 
-	std::vector<std::vector<std::vector<BlockGrid>*>*> blockGrids;
+	std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*> blockGrids;
 
 	std::unique_ptr<BlockGrid> bestGrid;
 	unsigned int bestScore;
 
 	unsigned int depth;
 	unsigned int worstNumberOfBlocks;
-	unsigned int worstHash;
 	unsigned int dbSize;
 
 public:
@@ -23,8 +53,8 @@ public:
 
 private:
 	void SolveDepth(unsigned int maxDBSize, bool& stop, bool dontAddToDB = false);
-	unsigned int SolveBlockGrid(const BlockGrid& blockGrid, unsigned int numberOfBlocks, unsigned int hash,
-		std::vector<std::vector<std::vector<BlockGrid>*>*>& newBlockGrids,
-		unsigned int& newWorstNumberOfBlocks, unsigned int& newWorstHash, bool& stop, bool dontAddToDB = false);
-	bool IsInDatabase(const BlockGrid& blockGrid, unsigned int numberOfBlocks, unsigned int hash, std::vector<std::vector<std::vector<BlockGrid>*>*>& newBlockGrids) const;
+	unsigned int SolveBlockGrid(const BlockGrid& blockGrid, unsigned int numberOfBlocks,
+		std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*>& newBlockGrids,
+		unsigned int& newWorstNumberOfBlocks, bool& stop, bool dontAddToDB = false);
+	bool IsInDatabase(const BlockGrid& blockGrid, unsigned int numberOfBlocks, std::vector<std::unordered_set<BlockGrid, BlockGridHash, BlockGridEqualTo>*>& newBlockGrids) const;
 };
