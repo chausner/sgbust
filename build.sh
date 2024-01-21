d1c9d46af864c235339031845fcfda35ad2abebf
chausner <chausner@users.noreply.github.com>
2024-01-21 23:23:18 +0100

    Minor tweaks to CMakeLists.txt and build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ export CXX=g++-13
 mkdir build
 cd build
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE=/mnt/d/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=/mnt/d/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 
