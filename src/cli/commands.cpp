d85701f297bf3a9d2525d756129f7bea297e8cb7
Christoph Hausner <christoph_hausner@hotmail.de>
2024-06-10 22:21:21 +0200

    Improvements and tweaks to benchmark command

--- src/cli/commands.cpp
+++ src/cli/commands.cpp
@@ -4,10 +4,11 @@
 #include <array>
 #include <chrono>
 #include <fstream>
+#include <future>
 #include <iostream>
 #include <optional>
 #include <random>
-#include <thread>
+#include <stdexcept>
 #include <unordered_set>
 
 #include "CLI/CLI.hpp"
@@ -153,7 +154,6 @@ void RunCommand(const BenchmarkCLIOptions& cliOptions)
 
     std::cout << "Press Ctrl+C to cancel." << std::endl;
 
-    unsigned long long gridsProcessed = 0;
     unsigned long long gridsSolved = 0;
     unsigned long long gridsCleared = 0;
     long long bestScoreSum = 0;
@@ -164,45 +164,41 @@ void RunCommand(const BenchmarkCLIOptions& cliOptions)
 
     auto printStats = [&]() {
         if (lastStatsPrinted.has_value())
-            std::cout << "\x1B[6F"; // move cursor to the beginning of the line six lines up
+            std::cout << "\x1B[5F"; // move cursor to the beginning of the line five lines up
 
         auto elapsed = std::chrono::steady_clock::now() - startTime;
         double elapsedSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(elapsed).count();
         auto zeroIfNaN = [](auto x) { return std::isnan(x) ? 0 : x; };
-        double gridsPerSecond = zeroIfNaN(gridsProcessed / elapsedSeconds);
-        double secondsPerGrid = zeroIfNaN(elapsedSeconds / gridsProcessed);
-        double gridsSolvedPercent = zeroIfNaN(static_cast<double>(gridsSolved) / gridsProcessed * 100);
-        double gridsClearedPercent = zeroIfNaN(static_cast<double>(gridsCleared) / gridsProcessed * 100);
+        double gridsPerSecond = zeroIfNaN(gridsSolved / elapsedSeconds);
+        double secondsPerGrid = zeroIfNaN(elapsedSeconds / gridsSolved);
+        double gridsClearedPercent = zeroIfNaN(static_cast<double>(gridsCleared) / gridsSolved * 100);
         double averageScore = zeroIfNaN(static_cast<double>(bestScoreSum) / gridsSolved);
         double averageBlocksRemaining = zeroIfNaN(static_cast<double>(blocksRemainingSum) / gridsSolved);
 
         std::cout << std::fixed << std::setprecision(2);
         std::cout << "Elapsed: " << elapsedSeconds << " seconds\x1B[K\n";
-        std::cout << "Grids processed: " << gridsProcessed << "\x1B[K\n";
-        std::cout << "Grids solved: " << gridsSolved << " (" << gridsSolvedPercent << "%)\x1B[K\n";
+        std::cout << "Grids solved: " << gridsSolved << "\x1B[K\n";
         std::cout << "Grids cleared: " << gridsCleared << " (" << gridsClearedPercent << "%)\x1B[K\n";
         std::cout << "Average score: " << averageScore << "\x1B[K\n";
         std::cout << "Average number of blocks remaining: " << averageBlocksRemaining << "\x1B[K\n";
         std::cout << "Speed: " << gridsPerSecond << " grids/second (" << secondsPerGrid << " seconds/grid)\x1B[K" << std::flush;
         };
 
-    std::thread thread([&] {
-        while (!cliOptions.NumGrids.has_value() || gridsProcessed < *cliOptions.NumGrids)
+    std::future<void> process = std::async([&] {
+        while (!cliOptions.NumGrids.has_value() || gridsSolved < *cliOptions.NumGrids)
         {
             sgbust::Grid blockGrid = sgbust::Grid::GenerateRandom(cliOptions.Width, cliOptions.Height, cliOptions.NumColors, mt);
 
             auto result = solver.Solve(blockGrid, cliOptions.MinGroupSize, *cliOptions.ScoringOptions.Scoring);
 
-            if (result.has_value())
-            {
-                gridsSolved++;
-                bestScoreSum += result->BestScore;
-                if (result->SolutionGrid.IsEmpty())
-                    gridsCleared++;
-                blocksRemainingSum += result->SolutionGrid.GetNumberOfBlocks();
-            }
+            if (!result.has_value())
+                throw std::logic_error("Solver::Solve unexpectedly returned std::nullopt");
 
-            gridsProcessed++;
+            gridsSolved++;
+            bestScoreSum += result->BestScore;
+            if (result->SolutionGrid.IsEmpty())
+                gridsCleared++;
+            blocksRemainingSum += result->SolutionGrid.GetNumberOfBlocks();
 
             auto now = std::chrono::steady_clock::now();
             if (!lastStatsPrinted.has_value() || now - *lastStatsPrinted >= RefreshInterval)
@@ -213,7 +209,7 @@ void RunCommand(const BenchmarkCLIOptions& cliOptions)
         }
         });
 
-    thread.join();
+    process.get();
 
     printStats();
     std::cout << std::endl;
