b2a088f76ed1b3cb37a6283cfec3fdc904cf25be
Christoph Hausner <christoph_hausner@hotmail.de>
2025-09-11 22:20:45 +0200

    Tweak formatting of elapsed time

--- src/cli/commands.cpp
+++ src/cli/commands.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <array>
 #include <chrono>
+#include <format>
 #include <fstream>
 #include <future>
 #include <iostream>
@@ -39,14 +40,13 @@ void RunCommand(const SolveCLIOptions& cliOptions)
 
     std::optional<sgbust::SolverResult> solverResult = solver.Solve(grid, minGroupSize, *cliOptions.ScoringOptions.Scoring, sgbust::Solution(cliOptions.SolutionPrefix));
 
-    auto endTime = std::chrono::steady_clock::now();
-
-    auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    auto elapsed = std::chrono::steady_clock::now() - startTime;
+	auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
 
     if (!cliOptions.Quiet)
     {
         std::cout << std::endl;
-        std::cout << "Done! - took " << elapsedMilliseconds << "ms" << std::endl;
+        std::cout << "Elapsed: " << std::format("{:%T}", elapsedMilliseconds) << std::endl;
         if (solverResult.has_value())
         {
             std::cout << "Best solution (score: " << solverResult->BestScore
@@ -165,6 +165,7 @@ void RunCommand(const BenchmarkCLIOptions& cliOptions)
             std::cout << "\x1B[5F"; // move cursor to the beginning of the line five lines up
 
         auto elapsed = std::chrono::steady_clock::now() - startTime;
+        auto elapsedMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed);
         double elapsedSeconds = std::chrono::duration_cast<std::chrono::duration<double>>(elapsed).count();
         auto zeroIfNaN = [](auto x) { return std::isnan(x) ? 0 : x; };
         double gridsPerSecond = zeroIfNaN(gridsSolved / elapsedSeconds);
@@ -174,7 +175,7 @@ void RunCommand(const BenchmarkCLIOptions& cliOptions)
         double averageBlocksRemaining = zeroIfNaN(static_cast<double>(blocksRemainingSum) / gridsSolved);
 
         std::cout << std::fixed << std::setprecision(2);
-        std::cout << "Elapsed: " << elapsedSeconds << " seconds\x1B[K\n";
+        std::cout << "Elapsed: " << std::format("{:%T}", elapsedMilliseconds) << "\x1B[K\n";
         std::cout << "Grids solved: " << gridsSolved << "\x1B[K\n";
         std::cout << "Grids cleared: " << gridsCleared << " (" << gridsClearedPercent << "%)\x1B[K\n";
         std::cout << "Average score: " << averageScore << "\x1B[K\n";
